70da86f36244aade8f18e296ff488d8f777c1713
dewanakl <dewanakretarta29@gmail.com>
2024-11-11 00:23:24 +0700

    feat: improve ui

--- js/guest.js
+++ js/guest.js
@@ -79,7 +79,7 @@ export const guest = (() => {
         } else {
             const div = document.createElement('div');
             div.classList.add('m-2');
-            div.innerHTML = `<p class="mt-0 mb-1 mx-0 p-0" style="font-size: 0.95rem;">${guest.getAttribute('data-message')}</p><h2>${util.escapeHtml(name)}</h2>`;
+            div.innerHTML = `<p class="mt-0 mb-1 mx-0 p-0" style="font-size: 0.95rem;">${guest.getAttribute('data-message')}</p><h2 class="m-0 p-0">${util.escapeHtml(name)}</h2>`;
             guest.appendChild(div);
         }
 
