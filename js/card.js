a6da4ef79c6d7423770453f94feb081d23413f39
dewanakl <dewanakretarta29@gmail.com>
2024-12-07 22:50:44 +0700

    feat: add role button

--- js/card.js
+++ js/card.js
@@ -149,7 +149,7 @@ export const card = (() => {
         </div>
         <hr class="text-${text} my-1">
         <p class="text-${text} my-1 mx-0 p-0" style="white-space: pre-wrap !important; font-size: 0.95rem;" ${moreThan200 ? `data-comment="${util.base64Encode(original)}"` : ''} id="content-${comment.uuid}">${moreThan200 ? (original.slice(0, 200) + '...') : original}</p>
-        ${moreThan200 ? `<p class="mb-2 mt-0 mx-0 p-0"><a class="text-${text}" style="font-size: 0.85rem;" data-show="false" onclick="comment.showMore(this, '${comment.uuid}')">Selengkapnya</a></p>` : ''}`;
+        ${moreThan200 ? `<p class="mb-2 mt-0 mx-0 p-0"><a class="text-${text}" role="button" style="font-size: 0.85rem;" data-show="false" onclick="comment.showMore(this, '${comment.uuid}')">Selengkapnya</a></p>` : ''}`;
     };
 
     const renderContent = (comment, is_parent) => {
